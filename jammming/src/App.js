86ecf802e51d9d1dfba56c6c9e0e03911a404b10
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-13 13:01:28 -0500

    Adjust header height for better view

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +19,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
       </header>
       <div>
         <SearchBar/>
