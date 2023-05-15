4129b7d948f79f96deb069d2c777bc68a658ec53
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-15 10:33:07 -0500

    Add ID attribute to Tracklist

--- jammming/src/Tracklist/Tracklist.js
+++ jammming/src/Tracklist/Tracklist.js
@@ -10,9 +10,9 @@ import Track from '../Track/Track';
 function Tracklist(props) {
     return (
         <div>
-            <Track title="Start Your Engines" artist="Fourbanger"/>
-            <Track title="The Mole" artist="Hans Zimmer"/>
-            <Track title="A Far Green Country" artist="Howard Shore"/>
+            <Track title="Start Your Engines" artist="Fourbanger" album="Fourbanger" id="1"/>
+            <Track title="The Mole" artist="Hans Zimmer" album="Dunkirk" id="2"/>
+            <Track title="A Far Green Country" artist="Howard Shore" album="The Lord of the Rings: Return of the King" id="3"/>
         </div>
     )
 }
